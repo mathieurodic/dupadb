9f9d7a60fcbecf58f72c1ee1b3ee9b5e428b826b
Mathieu Rodic <mathieu@rodic.fr>
2015-12-14 15:11:05 +0100

    Reorganized 'util' folder  - integrated `number2exression` into 'util/generators.hpp'  - integrated `str_t` into new 'util/types.hpp'

--- src/util/generators.hpp
+++ src/util/generators.hpp
@@ -2,6 +2,7 @@
 #define __INCLUDED__utils__generators_hpp__
 
 
+#include <string>
 #include <stdlib.h>
 
 template<int length>
@@ -22,6 +23,52 @@ inline const char* generate_gibberish() {
     ending_position = rand() % length;
     gibberish[ending_position] = '\0';
     return gibberish;
+}
+
+const std::string number2expression(uint32_t number) {
+    static const uint32_t block_values[4] = {1000000000, 1000000, 1000, 1};
+    static const std::string block_names[4] = {" billion", " million", " thousand", ""};
+    static const std::string unities[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    static const std::string specials[10] = {"ten", "eleven", "twelve", "thirteen", "forteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    static const std::string tens[10] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    std::string expression;
+    for (uint32_t b=0; b<4; b++) {
+        uint16_t block = (number / block_values[b]) % 1000;
+        if (block == 0) {
+            continue;
+        }
+        uint8_t n0 = block % 10;
+        block /= 10;
+        uint8_t n1 = block % 10;
+        block /= 10;
+        uint8_t n2 = block % 10;
+        if (n2) {
+            if (!expression.empty()) {
+                expression += ' ';
+            }
+            expression += unities[n2] + " hundred";
+        }
+        if (n1) {
+            if (!expression.empty()) {
+                expression += ' ';
+            }
+            if (n1 == 1) {
+                expression += specials[n0];
+            } else {
+                expression += tens[n1];
+            }
+        }
+        if (n0 && n1 != 1) {
+            if (n1) {
+                expression += '-';
+            } else if (!expression.empty()) {
+                expression += ' ';
+            }
+            expression += unities[n0];
+        }
+        expression += block_names[b];
+    }
+    return expression.empty() ? unities[0] : expression;
 }
 
 
---
+++ src/util/types.hpp
@@ -0,0 +1,83 @@
+#ifndef __INCLUDED__utils__types_hpp__
+#define __INCLUDED__utils__types_hpp__
+
+
+#include <unordered_map>
+
+
+template<uint32_t size=256>
+struct str_t {
+    char _data[size];
+    inline str_t() {
+        memset(_data, 0, size);
+    }
+    inline str_t(const char* source) {
+        strncpy(_data, source, size);
+    }
+    inline str_t(const str_t<size>& source) {
+        strncpy(_data, source._data, size);
+    }
+
+    inline const int comp(const str_t<size>& other) const {
+        return memcmp(_data, other._data, size);
+    }
+
+    inline bool operator != (const char* other) const {
+        return strncmp(_data, other, size) != 0;
+    }
+    inline bool operator != (const std::string& other) const {
+        return strncmp(_data, other.c_str(), size) != 0;
+    }
+
+    inline const bool operator < (const str_t<size>& other) const {
+        return (comp(other) < 0);
+    }
+    inline const bool operator <= (const str_t<size>& other) const {
+        return (comp(other) <= 0);
+    }
+    inline const bool operator == (const str_t<size>& other) const {
+        return (comp(other) == 0);
+    }
+    inline const bool operator >= (const str_t<size>& other) const {
+        return (comp(other) >= 0);
+    }
+    inline const bool operator > (const str_t<size>& other) const {
+        return (comp(other) > 0);
+    }
+    inline const str_t<size>& operator = (const str_t<size>& source) {
+        memcpy(_data, source._data, size);
+        return *this;
+    }
+    inline const str_t<size>& operator = (const char* source) {
+        strncpy(_data, source, size);
+        return *this;
+    }
+    inline const str_t<size>& operator = (const std::string source) {
+        strncpy(_data, source.c_str(), size);
+        return *this;
+    }
+    inline void clear() {
+        memset(_data, 0, size);
+    }
+    inline const char* data() const {
+        return _data;
+    }
+};
+
+namespace std {
+    template<uint32_t size>
+    struct hash<str_t<size>> {
+        std::size_t operator()(const str_t<size>& str) const {
+            const char* data = str.data();
+            std::size_t hash = 31;
+            while (*data) {
+                hash = (hash * 54059) ^ (data[0] * 76963);
+                data++;
+            }
+            return hash;
+        }
+    };
+}
+
+
+#endif // __INCLUDED__utils__types_hpp__
